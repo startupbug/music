9ea73d7dad5a41311976bbc9c6143ef982850a27
aliaimviz <im.irfanmumtaz21@gmail.com>
2018-03-30 15:29:53 +0500

    pulling

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -80,17 +80,17 @@ class RegisterController extends Controller
                 'password' => bcrypt($data['password']),
                 'role_id' => $data['role_id'],
                 'email_token' => str_random(10),
-                ]);
+            ]);
 
 
-                //Updating unique id of User
-                $update_uniqueid = User::find($user->id);
-                $update_uniqueid->promoter_affiliated_id = $user->id.Hash::make(str_random(5));
-                $update_uniqueid->save();
+            //Updating unique id of User
+            $update_uniqueid = User::find($user->id);
+            $update_uniqueid->promoter_affiliated_id = $user->id.Hash::make(str_random(5));
+            $update_uniqueid->save();
 
-                // Mail::to($data['email'])->send(new EmailVerification($user));
-                // auth()->login($user);
-                $email = new EmailVerification(new User(['email_token' => $user->email_token, 'name' => $user->name, 'email'=> $user->email]));
+            // Mail::to($data['email'])->send(new EmailVerification($user));
+            // auth()->login($user);
+            $email = new EmailVerification(new User(['email_token' => $user->email_token, 'name' => $user->name, 'email'=> $user->email]));
             Mail::to($user->email)->send($email);
             DB::commit();
             Session::flash('message', 'We have sent you a verification email!');
