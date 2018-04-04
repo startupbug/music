a2d8e45062c1f728ddb1d98ecaf5f803ed8c5370
Sheheryar Nadeem <Sheheryar.n@aimviz.com>
2018-04-04 18:25:59 +0500

    Push

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -10,6 +10,19 @@
 //      }
 // })
 
+var $radios = $('input[name=optradio_contest]').change(function () {
+    var value = $radios.filter(':checked').val();
+    console.log(value);
+    if (value == "select") {
+      $("#combox_song").css("display", "block");
+      $("#file_song").css("display", "none");
+    }
+    else if(value == "file") {
+      $("#combox_song").css("display", "none");
+      $("#file_song").css("display", "block");
+    }
+});
+
 
   $(".image_thumbnail").click(function(){
       if ($(this).siblings()[0].paused == false) {
